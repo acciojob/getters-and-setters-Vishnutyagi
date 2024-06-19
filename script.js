b69b3b898a4b91c59915e2eba4b90575875ac788
Vishnu tyagi <106895072+Vishnutyagi@users.noreply.github.com>
2024-06-19 14:54:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,37 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name,age){
+		this._name=name;
+		this._age=age;
+	}
+	get name(){
+		return this._name;
+	}
+	set age(age){
+		this._age=age;
+	}
+	 get age() {
+        return this._age;
+    }
+}
 
-class Student extends Person {}
+class Student extends Person {
+	constructor(name,age){
+		super(name,age);
+	}
+	study(){
+		console.log(`${this._name} is studying`);
+	}
+}
 
-class Teacher extends Person {}
-
+class Teacher extends Person {
+	constructor(name,age){
+		super(name,age);
+	}
+	teach(){
+		console.log(`${this._name} is teaching`);
+	}
+}
 // Do not change the code below this line
 window.Person = Person;
 window.Student = Student;
